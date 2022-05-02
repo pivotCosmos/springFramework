9470eca576e800c289290e88a731430f1b9a93d7
jeongah <99481661+pivotCosmos@users.noreply.github.com>
2022-05-02 18:29:39 +0900

    DispatcherServlet 직접 만들기

---
+++ step07_MyFramework/src/main/java/kosta/mvc/controller/UpdateController.java
@@ -0,0 +1,24 @@
+package kosta.mvc.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UpdateController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("UpdateController 입니다...");
+		
+		request.setAttribute("message", "수정완료 내용입니다....");
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("updateResult.jsp");
+		
+		return mv;
+	}
+
+}
